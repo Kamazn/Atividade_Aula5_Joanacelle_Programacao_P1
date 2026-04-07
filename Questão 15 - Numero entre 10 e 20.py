35006cfc822e9f73354ebc41fde3dfa5bec73c24
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-04-07 04:20:49 -0300

    Delete Questão 15 - Numero entre 10 e 20.py

--- Questão 15 - Numero entre 10 e 20.py
+++
@@ -1,7 +0,0 @@
-# Questão 15 - Leia um número e: Se estiver entre 10 e 20 → “Dentro”; Caso contrário → “Fora”
-numero = float(input("Questão 15 - Digite um número para saber se está entre 10 e 20: ")) # Recebe o número do usuário 
-
-if (numero >= 10) and (numero <= 20): # Verifica se o numero fica entre 10 e 20
-               print("Dentro") # Fica entre 10 e 20
-else: # Se não for 
-               print("Fora") # Não fica entre 10 e 20
