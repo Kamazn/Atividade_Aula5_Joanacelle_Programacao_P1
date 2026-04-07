198df7937cfa7b5af2afaa15c8aa3575fd63007e
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-04-07 04:18:20 -0300

    Delete Questão 5 - Triplo de numero.py

--- Questão 5 - Triplo de numero.py
+++
@@ -1,6 +0,0 @@
-# Questão 5 - Leia um número como texto (input) e: Converta para inteiro; Mostre o triplo.
-numero = input("Questão 5 - Digite um número para calcular o triplo: ") # Recebe o número do usuário 
-numero = int(numero) # Converte para inteiro
-
-resultado = numero * 3 # Calcula o triplo do número
-print("Resultado:", resultado) # Mostra o resultado 
