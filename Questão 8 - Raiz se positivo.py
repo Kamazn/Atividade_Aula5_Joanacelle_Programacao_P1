3b9fdbe5bc3e2234a8433f45b453dd67fe2812d2
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-04-07 04:18:37 -0300

    Delete Questão 8 - Raiz se positivo.py

--- Questão 8 - Raiz se positivo.py
+++
@@ -1,7 +0,0 @@
-# Questão 8 - Leia um número e: Se for positivo, mostre a raiz aproximada (use **0.5); Caso contrário, informe “Número inválido”
-numero = float(input("Questão 8 - Digite um número positivo para saber a raiz aproximada: ")) # Recebe o número do usuário 
-
-if numero > 0:
-    print("A raiz quadrada aproximada do seu número é:",numero ** 0.5)
-else:
-    print("Número inválido, digite outro.")
