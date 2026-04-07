7db85a7b6502e951953dcd0fb0b2f5871b559316
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-04-07 04:17:59 -0300

    Delete Questão 2 - Dobro de numero.py

--- Questão 2 - Dobro de numero.py
+++
@@ -1,6 +0,0 @@
-# Questão 2 - Leia um número (entrada padrão do input) e: Converta para inteiro; Exiba o dobro do valor.
-numero = (input("Questão 2 - Digite um número inteiro para calcular o dobro: ")) # Recebe o número do usuario como string
-numero = int(numero) # Converte para int
-
-resultado = numero * 2 # Calcula o dobro do número
-print("Resultado:", resultado, "Tipo:", type(numero).__name__) # Mostra o resultado e o tipo da variável
