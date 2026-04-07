140b11505515f904287f314c1259a9d1a3c2f997
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-04-07 04:18:43 -0300

    Delete Questão 9 - Metade do valor.py

--- Questão 9 - Metade do valor.py
+++
@@ -1,5 +0,0 @@
-# Questão 9 - Leia um valor e: Converta para float; Mostre a metade
-numero = input("Questão 9 - Digite um número para dividir pela metade: ") # Recebe o número do usuario como string
-numero = float(numero) # Converte para float
-
-print("Resultado:", numero / 2)
